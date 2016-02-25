001730a7bbdd4e440541cd851843c8e4a92f53a9
Jason Harab <j.harabbi@gmail.com>
2016-02-25 22:21:34 +0000

    Add missing 'end'

--- app/mailers/staff_mailer.rb
+++ app/mailers/staff_mailer.rb
@@ -7,4 +7,5 @@ class StaffMailer < ApplicationMailer
   def faq_added_email(faq_id)
     @faq = Faq.find(faq_id)
     mail(to: User.select('email').where(is_staff: true).map(&:email), subject: 'New Question Posted to the FAQ')
+  end
 end
